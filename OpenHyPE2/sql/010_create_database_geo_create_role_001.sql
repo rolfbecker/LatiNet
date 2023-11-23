2f7a7764d24dd088a992e5762a954f30cf01e682
Rolf Becker <rolf.becker@hsrw.eu>
2023-11-23 04:19:02 +0100

    Update

---
+++ OpenHyPE2/sql/010_create_database_geo_create_role_001.sql
@@ -0,0 +1,28 @@
+
+-- connect as user postgres to the maintenance database postgres and create the database geo.
+-- psql -U postgres -d postgres -f 010_create_database_geo_create_role_001.sql
+--
+-- In case you have to be postgres user on your os to be able to connect use:
+--
+-- sudo -u postgres psql -f 010_create_database_geo_create_role_001.sql
+
+CREATE ROLE geo_master WITH
+	LOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	CREATEROLE
+	INHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1
+	PASSWORD 'xxxxxx';
+COMMENT ON ROLE geo_master IS 'The geo database master user.';
+
+CREATE DATABASE geo
+    WITH 
+    OWNER = geo_master
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE geo
+    IS 'Geospatial database for training';
+	
--- OpenHyPE2/sql/README.md
+++ OpenHyPE2/sql/README.md
@@ -1,6 +1,12 @@
 ====== Create PostgreSQL Environmental Database ======
 
 ```
+# Database geo
+sudo -u postgres psql -f 010_create_database_geo_create_role_001.sql
+```
+
+```
+# Alternative: Database env_db
 sudo -u postgres psql -f 010_create_users_for_env_db_V001.sql 
 sudo -u postgres psql -f 020_create_database_env_db_V001.sql 
 ```
